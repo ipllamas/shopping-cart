5322836ea379434f628c0c5d0980859e6739c37d
Ivan Llamas <ip.llamas13@gmail.com>
2022-11-24 22:43:59 -0800

    add routes to Home and Shop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
 
 const App = () => {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/shop" element={<Shop/>} />
+    </Routes>
+  </BrowserRouter>
 
 }
 
