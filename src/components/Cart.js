ae5ee5da2806b9b3b5f629fb51ad4bb4d40cf1da
Ivan Llamas <ip.llamas13@gmail.com>
2022-12-08 00:30:50 -0800

    refactor cart modal toggle with React hooks

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,16 +1,22 @@
 const Cart = (props) => {
-  const cartItems = props.cartItems;
+  //const cartItems = props.cartItems;
+  const cartShown = props.cartShown;
 
-  return (
-    <div className='shoppingCart'>
-      <div className="cartContainer">
-        <h1>Your Cart</h1>
-        <div className="cartList">
-          <p>{cartItems.length === 0 && 'Your cart is empty.'}</p>
+  if(!cartShown) {
+    return null;
+  }else {
+    return (
+      <div className='shoppingCart'>
+        <div className="cartContainer">
+          <h1>Your Cart</h1>
+          {/* <div className="cartList">
+            <p>{cartItems.length === 0 && 'Your cart is empty.'}</p>
+          </div> */}
         </div>
       </div>
-    </div>
-  )
+    )
+  }  
+
 }
 
 export default Cart;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,17 +2,14 @@ import {Link} from "react-router-dom";
 
 const Nav = (props) => {
   
-  const cartToggle = () => {
-    const cartSelector = document.querySelector('.shoppingCart');
-    cartSelector.classList.toggle('visible')
-  }
+  const toggleCart = props.toggleCart;
 
   return (
     <div className="navbar">
       <ul>
         <li className="navItem"><Link to="/">Home</Link></li>
         <li className="navItem"><Link to="/shop">Shop</Link></li>
-        <li className="navItem"><button onClick={cartToggle}>Cart</button></li>
+        <li className="navItem"><button onClick={toggleCart}>Cart</button></li>
       </ul>
     </div>
   );
